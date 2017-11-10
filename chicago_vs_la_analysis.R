e80aeddd8110031ae3e7dd60c11779316f53df5b
rhawks7 <apparaokalimireddy@gmail.com>
2017-11-10 00:58:31 -0600

    Updates

--- chicago_vs_la_analysis.R
+++ chicago_vs_la_analysis.R
@@ -12,7 +12,7 @@ sql("use bighawk")
 chicagoCrimes<-sql("select c.*, month(crime_date) as month, date_format(crime_date, 'u') as day, hour(crime_date) as hour, a.community as community from chicago_crimes_part_year c left join chicago_communities a on (c.community_area = a.community_area)")
 
 # Get all crime data from la_crimes_part_year tables
-laCrimes<-sql("select * from la_crimes_part_year")
+laCrimes<-sql("select *, int(substr(date_occurred, 1, 2)) as month from la_crimes_part_year")
 
 ##
 ## Function:compareCrimes
@@ -41,3 +41,28 @@ compareCrimes<-function() {
   theme_bw(base_size = 14)
   ggsave(file="chicago-la.png")
 }
+
+##
+## Function: crimeTrendByMonth(years)
+##
+laCrimeTrendByMonth<-function(years) {
+  # Draw a line chart of a crime
+  p<-ggplot()
+  p<-p+xlab("Month") + ylab("Crimes")
+  p<-p+ggtitle("Trend of Crimes By Month in LA")
+  p<-p+theme(plot.title = element_text(hjust = 0.5, face="bold"))
+  p<-p+scale_y_continuous(labels = comma)
+  p<-p+scale_x_continuous(breaks=seq(1,12,by=1))
+  for (y in years) {
+    df<-as.data.frame(count(groupBy(where(laCrimes,laCrimes$part_year==y),"part_year", "month")))
+    colnames(df) <- c("Year","Month","Count")
+    p<-p+geom_line(data=df, aes(color=factor(Year), group=factor(Year), x=Month,y=Count), size=1.5)
+    p<-p+labs(color = "Year")
+  }
+  ggsave(file=paste("la_crime_trend_by_month", ".png", sep=""), plot=p)
+}
+
+
+# Run Analysis
+compareCrimes()
+laCrimeTrendByMonth(years=c(2014,2015,2016))
